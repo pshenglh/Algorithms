f0d37cb52e4414619e7ffa0515b0a220fe004526
pshenglh <psheng@outlook.com>
2017-07-04 10:46:16 +0800

    Shell-Sort

--- src/SortCompare.java
+++ src/SortCompare.java
@@ -10,6 +10,7 @@ public class SortCompare {
         Stopwatch timer = new Stopwatch();
         if (alg.equals("Insertion")) Insertion.sort(a);
         if (alg.equals("Selection")) Selection.sort(a);
+        if (alg.equals("Shell")) Shell.sort(a);
         return timer.elapsedTime();
     }
     public static double timeRandomInput(String alg, int N, int T)
