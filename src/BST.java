cae771028b582e733e3f0f228fdd91bee7e01433
pshenglh <psheng@outlook.com>
2017-07-14 23:30:40 +0800

    SymbolGraph

--- src/BST.java
+++ src/BST.java
@@ -1,7 +1,10 @@
+import edu.princeton.cs.algs4.StdOut;
+
 /**
  * Created by pshen on 2017/7/11.
  */
-public class BST<Key extends Comparable<Key>, Value> {
+public class BST<Key extends Comparable<Key>, Value>
+{
     private Node root;
 
     private class Node
@@ -11,166 +14,177 @@ public class BST<Key extends Comparable<Key>, Value> {
         private Node left, right;
         private int N;
 
-        public Node(Key key, Value val, int N)
-        { this.key = key; this.val = val; this.N = N; }
+        public Node(Key key, Value val, int N) {
+            this.key = key;
+            this.val = val;
+            this.N = N;
+        }
+    }
 
-        public int size()
-        { return size(root); }
-
-        private int size(Node x)
+    public int size()
         {
-            if (x == null) return 0;
-            else return x.N;
+            return size(root);
         }
 
-        private Value get(Node x, Key key)
-        {
-            if (x == null) return null;
-            int cmp = key.compareTo(x.key);
-            if (cmp < 0) return get(x.left, key);
-            else if (cmp > 0) return get(x.right, key);
-            else return x.val;
-        }
+    private int size(Node x)
+    {
+        if (x == null) return 0;
+        else return x.N;
+    }
 
-        public Value get(Key key)
+    private Value get(Node x, Key key)
+    {
+        if (x == null) return null;
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0) return get(x.left, key);
+        else if (cmp > 0) return get(x.right, key);
+        else return x.val;
+    }
+
+    public Value get(Key key)
         {
             return get(root, key);
         }
 
-        private Node put(Node x, Key key, Value val)
+    private Node put(Node x, Key key, Value val)
+    {
+        if (x == null) return new Node(key, val, 1);
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0) x.left =  put(x.left, key, val);
+        else if (cmp > 0) x.right = put(x.right, key, val);
+        else x.val = val;
+        x.N = size(x.left) + size(x.right) + 1;
+        return x;
+    }
+
+    public void put(Key key, Value val)
+    { root = put(root, key, val); }
+
+    private Node min(Node x)
+    {
+        if (x.left == null )
         {
-            if (x == null) return new Node(key, val, 1);
-            int cmp = key.compareTo(x.key);
-            if (cmp < 0) return put(x.left, key, val);
-            else if (cmp > 0) return put(x.right, key, val);
-            else x.val = val;
-            x.N = size(x.left) + size(x.right) + 1;
             return x;
         }
+        return min(x.left);
+    }
 
-        public Node put(Key key, Value val)
-        { return put(root, key, val); }
+    public Key min()
+    { return min(root).key; }
 
-        private Node min(Node x)
-        {
-            if (x.left == null ) return x;
-            return min(x.left);
-        }
+    private Node max(Node x)
+    {
+        if (x.right == null) return x;
+        return max(x.right);
+    }
 
-        public Key min()
-        { return min(root).key; }
+    public Key max()
+    { return max(root).key; }
 
-        private Node max(Node x)
-        {
-            if (x.right == null) return x;
-            return max(x.right);
-        }
+    private Node floor(Node x, Key key)
+    {
+        if (x == null) return null;
+        int cmp = key.compareTo(x.key);
+        if (cmp == 0) return x;
+        if (cmp < 0) return floor(x.left, key);
+        Node t = floor(x.right, key);
+        if (t != null) return t;
+        else return x;
+    }
 
-        public Key max()
-        { return max(root).key; }
+    public Key floor(Key key)
+    {
+        Node x = floor(root, key);
+        if (x == null) return null;
+        return x.key;
+    }
 
-        private Node floor(Node x, Key key)
-        {
-            if (x == null) return null;
-            int cmp = key.compareTo(x.key);
-            if (cmp == 0) return x;
-            if (cmp < 0) return floor(x.left, key);
-            Node t = floor(x.right, key);
-            if (t != null) return t;
-            else return x;
-        }
+    private Node ceiiling(Node x, Key key)
+    {
+        if (x == null) return null;
+        int cmp = key.compareTo(x.key);
+        if (cmp == 0) return x;
+        if (cmp > 0) return ceiiling(x.right, key);
+        Node t = ceiiling(x.left, key);
+        if (t != null) return t;
+        else return x;
+    }
 
-        public Key floor(Key key)
-        {
-            Node x = floor(root, key);
-            if (x == null) return null;
-            return x.key;
-        }
+    private Node select(Node x, int k)
+    {
+        if (x == null) return null;
+        int t = size(x.left);
+        if (t > k) return select(x.left, k);
+        if (t == k) return x;
+        else return select(x.right, k - t -1);
+    }
 
-        private Node ceiiling(Node x, Key key)
-        {
-            if (x == null) return null;
-            int cmp = key.compareTo(x.key);
-            if (cmp == 0) return x;
-            if (cmp > 0) return ceiiling(x.right, key);
-            Node t = ceiiling(x.left, key);
-            if (t != null) return t;
-            else return x;
-        }
+    public Key select(int k)
+    { return select(root, k).key; }
 
-        private Node select(Node x, int k)
-        {
-            if (x == null) return null;
-            int t = size(x.left);
-            if (t > k) return select(x.left, k);
-            if (t == k) return x;
-            else return select(x.right, k - t -1);
-        }
+    private int rank(Node x, Key key)
+    {
+        if (x == null) return 0;
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0) return rank(x.left, key);
+        else if (cmp > 0) return  rank(x.right, key) + size(x.left) + 1;
+        else return size(x.left);
+    }
 
-        public Key select(int k)
-        { return select(root, k).key; }
+    private Node deleteMin(Node x)
+    {
+        if (x.left == null) return x.right;
+        x.left = deleteMin(x.left);
+        x.N = size(x.left) + size(x.right) + 1;
+        return x;
+    }
 
-        private int rank(Node x, Key key)
-        {
-            if (x == null) return 0;
-            int cmp = key.compareTo(x.key);
-            if (cmp < 0) return rank(x.left, key);
-            else if (cmp > 0) return  rank(x.right, key) + size(x.left) + 1;
-            else return size(x.left);
-        }
+    public void deleteMin()
+    { root = deleteMin(root); }
 
-        private Node deleteMin(Node x)
+    private Node delete(Node x, Key key)
+    {
+        if (x == null) return null;
+        int cmp = key.compareTo(x. key);
+        if (cmp < 0) x.left = delete(x.left, key);
+        if (cmp > 0) x.right = delete(x.right, key);
+        else
         {
             if (x.left == null) return x.right;
-            x.left = deleteMin(x.left);
-            x.N = size(x.left) + size(x.right) + 1;
-            return x;
+            if (x.right == null) return x.left;
+            Node t = x;
+            x = min(t.right);
+            x.right = deleteMin(t.right);
+            x.left = t.left;
         }
-
-        public void deleteMin()
-        { root = deleteMin(root); }
-
-        private Node delete(Node x, Key key)
-        {
-            if (x == null) return null;
-            int cmp = key.compareTo(x. key);
-            if (cmp < 0) x.left = delete(x.left, key);
-            if (cmp > 0) x.right = delete(x.right, key);
-            else
-            {
-                if (x.left == null) return x.right;
-                if (x.right == null) return x.left;
-                Node t = x;
-                x = min(t.right);
-                x.right = deleteMin(t.right);
-                x.left = t.left;
-            }
-            x.N = size(x.left) + size(x.right) + 1;
-            return x;
-        }
-
-        public void delete(Key key)
-        { root = delete(root, key); }
-
-        public Iterable<Key> keys()
-        { return keys(min(), max()); }
-
-        public Iterable<Key> keys(Key lo, Key hi)
-        {
-            Queue<Key> queue = new Queue<Key>();
-            keys(root, queue, lo, hi);
-            return queue;
-        }
-
-        private void keys(Node x, Queue<Key> queue, Key lo, Key hi)
-        {
-
-            if (x == null) return;
-            int cmplo = lo.compareTo(x.key);
-            int cmphi = hi.compareTo(x.key);
-            if (cmplo < 0) keys(x.left, queue, lo, hi);
-            if (cmplo <= 0 && cmphi >= 0) queue.enqueue(x.key);
-            if (cmphi > 0) keys(x.right, queue, lo, hi);
-        }
+        x.N = size(x.left) + size(x.right) + 1;
+        return x;
     }
+
+    public void delete(Key key)
+    { root = delete(root, key); }
+
+    public Iterable<Key> keys()
+    { return keys(min(), max()); }
+
+    public Iterable<Key> keys(Key lo, Key hi)
+    {
+        Queue<Key> queue = new Queue<Key>();
+        keys(root, queue, lo, hi);
+        return queue;
+    }
+
+    private void keys(Node x, Queue<Key> queue, Key lo, Key hi)
+    {
+
+        if (x == null) return;
+        int cmplo = lo.compareTo(x.key);
+        int cmphi = hi.compareTo(x.key);
+        if (cmplo < 0) keys(x.left, queue, lo, hi);
+        if (cmplo <= 0 && cmphi >= 0) queue.enqueue(x.key);
+        if (cmphi > 0) keys(x.right, queue, lo, hi);
+    }
+
+    public boolean contains(Key key)
+    { return get(key) != null; }
 }
