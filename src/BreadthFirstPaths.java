d1170b6288324027fbeaab6e7f27e3922d29afaa
pshenglh <psheng@outlook.com>
2017-07-14 14:33:41 +0800

    BreadthFirstPaths

--- src/BreadthFirstPaths.java
+++ src/BreadthFirstPaths.java
@@ -2,43 +2,48 @@ import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdOut;
 
 /**
- * Created by pshen on 2017/7/13.
+ * Created by pshen on 2017/7/14.
  */
-public class DepthFirstPaths {
+public class BreadthFirstPaths {
     private boolean[] marked;
     private int[] edgeTo;
     private final int s;
 
-    public DepthFirstPaths(Graph G, int s)
+    public BreadthFirstPaths(Graph G, int s)
     {
         marked = new boolean[G.V()];
         edgeTo = new int[G.V()];
         this.s = s;
-        dfg(G, s);
+        bfs(G, s);
     }
 
-    private void dfg(Graph G, int v)
+    public void bfs(Graph G, int s)
     {
-        marked[v] = true;
-        for (int w : G.adj(v))
+        Queue<Integer> queue = new Queue<Integer>();
+        marked[s] = true;
+        queue.enqueue(s);
+        while (!queue.isEmpty())
         {
-            if (!marked[w])
-            {
-                edgeTo[w] = v;
-                dfg(G, w);
-            }
+            int v = queue.dequeue();
+            for (int w : G.adj(v))
+                if (!marked[w])
+                {
+                    edgeTo[w] = v;
+                    marked[w] = true;
+                    queue.enqueue(w);
+                }
         }
     }
 
     public boolean hasToPath(int v)
     { return marked[v]; }
 
-    public Iterable<Integer> pathTo(int v)
+    private Iterable<Integer> pathTo(int v)
     {
         if (!hasToPath(v)) return null;
         Stack<Integer> path = new Stack<Integer>();
-        for (int x = v; x != s; x = edgeTo[x])
-            path.push(x);
+        for (int i = v; i != s; i = edgeTo[i])
+            path.push(i);
         path.push(s);
         return path;
     }
@@ -46,9 +51,8 @@ public class DepthFirstPaths {
     public static void main(String[] args)
     {
         Graph G = new Graph(new In(args[0]));
-        StdOut.println(G);
         int s = Integer.parseInt(args[1]);
-        DepthFirstPaths search = new DepthFirstPaths(G, s);
+        BreadthFirstPaths search = new BreadthFirstPaths(G, s);
         for (int v = 0; v < G.V(); v++)
         {
             StdOut.print(s + " to " + v + ": ");
